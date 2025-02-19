536c9cf3bee1a65a984f37011e3e857c76164bde
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-02-19 17:19:52 +0200

    added tests

---
+++ app_cloning/javascript-amazon-project/tests/moneyTest.js
@@ -0,0 +1,26 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+if(formatCurrency(2095) === '20.95'){
+    console.log('Test passed.');
+}
+else
+{
+    console.log('Test failed.');
+}
+
+if(formatCurrency(0) === '0.00'){
+    console.log('Test passed.');
+}
+else
+{
+    console.log('Test failed.');
+}
+
+
+if(formatCurrency(2001) === '20.01'){
+    console.log('Test passed.');
+}
+else
+{
+    console.log('Test failed.');
+}
