93923d2448b54ae8bd2efc750d39ef6b603cdd1b
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-02-26 18:20:55 +0200

    last touches

--- app_cloning/javascript-amazon-project/scripts/checkout.js
+++ app_cloning/javascript-amazon-project/scripts/checkout.js
@@ -1,10 +1,30 @@
-import { renderOrderSummary} from "./checkout/orderSummary.js";
-import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
-    renderOrderSummary();
-    renderPaymentSummary();
-});
+async function loadPage() {
+  try {
+    // throw 'error1';
+
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve, reject) => {
+      // throw 'error2';
+      loadCart(() => {
+        // reject('error3');
+        resolve('value3');
+      });
+    });
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
