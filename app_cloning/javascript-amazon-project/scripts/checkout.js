399ad994579e5e9fbf8afc4c2fae46ca1fc8b115
rykyfilipe <161625737+rykyfilipe@users.noreply.github.com>
2025-03-01 13:30:12 +0200

    name update

--- app_cloning/javascript-amazon-project/scripts/checkout.js
+++ app_cloning/javascript-amazon-project/scripts/checkout.js
@@ -7,14 +7,10 @@ import {loadCart} from '../data/cart.js';
 
 async function loadPage() {
   try {
-    // throw 'error1';
-
     await loadProductsFetch();
 
     const value = await new Promise((resolve, reject) => {
-      // throw 'error2';
       loadCart(() => {
-        // reject('error3');
         resolve('value3');
       });
     });
